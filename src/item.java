1201812310f8455f7a388550f46981232156f167
Richard  Farmer <rtfarmer001@gmail.com>
2025-04-22 08:45:29 -0700

    item class

---
+++ src/item.java
@@ -0,0 +1,19 @@
+
+public class item {
+	private String itemName;
+	private String type;
+	private double price;
+	
+	public void newItem(String newItem) {
+		itemName = newItem;
+	}
+	public void itemType(String itemType) {
+		type = itemType;
+	}
+	public void setPrice(double itemPrice) {
+		price = itemPrice;
+	}
+		
+	
+
+}
