f9030ba083f3aabb836237eb697b12a44e73834e
Richard  Farmer <rtfarmer001@gmail.com>
2025-04-22 08:43:22 -0700

    Skeleton of main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,102 @@
-
+import java.util.Scanner;
 public class Main {
+	public static void displayMenu(Scanner scnr) {
+		System.out.println("-MENU-");
+		System.out.println("B - set Budget");
+		System.out.println("D - Display List and remaining budget");
+		System.out.println("O - Organization options");
+		System.out.println("T - Calculate total + tax");
+		System.out.println("Q - Quit");
+		System.out.println("Please choose an option: ");
+		executeMenu(scnr);
+	}
+	public static void executeMenu(Scanner scnr) {
+		String input;
+		Character choice;
 
+		do {
+			input = scnr.nextLine();
+			choice = input.charAt(0);
+			
+			if(choice.equals('b') || choice.equals('B')) {
+				setBudget(scnr);
+			}
+			else if(choice.equals('d') || choice.equals('D')) {
+				displayList(scnr);
+			}
+			else if(choice.equals('o') || choice.equals('O')) {
+				organizationOptions(scnr);
+			}
+			else if(choice.equals('t') || choice.equals('T')) {
+				calculateTotal(scnr);
+			}
+			else {
+				System.out.println("Please choose a valid option: ");
+			}
+
+		}while((!choice.equals('q')) && (!choice.equals('Q')));
+		return;
+	}
+	public static void setBudget(Scanner scnr) {
+		
+		displayMenu(scnr);
+	}
+	public static void displayList(Scanner scnr) {
+		
+		displayMenu(scnr);
+	}
+	public static void organizationOptions(Scanner scnr) {
+		Character choice;
+		String input;
+		
+		System.out.println("-ORGANIZATION OPTIONS-");
+		System.out.println("P - Organize by price");
+		System.out.println("G - Organize by group");
+		System.out.println("R - Return to menu");
+		System.out.println("Please choose an option: ");
+		
+		input = scnr.nextLine();
+		choice = input.charAt(0);
+		do {
+			if (choice.equals('p')) {
+				organizeByPrice(scnr);
+			}
+			else if(choice.equals('g')) {
+				organizeByGroup(scnr);
+			}
+			else{
+				System.out.println("Please enter a valid option: ");
+			}
+		} while ((!choice.equals('r') && (!choice.equals('R'))));
+		displayMenu(scnr);
+	}
+	public static void organizeByPrice(Scanner scnr) {
+	
+		displayMenu(scnr);
+	}
+	public static void organizeByGroup(Scanner scnr) {
+		
+		displayMenu(scnr);
+	}
+	public static void calculateTotal(Scanner scnr) {
+		
+		displayMenu(scnr);
+	}
+	public static void createItem(Scanner scnr) {
+		
+		addNewItemToArray();
+		displayMenu(scnr);
+	}
+	public static void addNewItemToArray () {
+		
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("This is an example of a commit!");
+		Scanner scnr = new Scanner(System.in);
+		
+		displayMenu(scnr);
+		
+		scnr.close();
 	}
 
 }
